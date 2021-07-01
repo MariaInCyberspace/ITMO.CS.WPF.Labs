b72fa5cfd591280881e5207e1540faae6f020ae1
Maria Spirid <lan194@hotmail.com>
2021-07-01 05:07:46 +0300

    Started working on my calculator

---
+++ MyCalc/MyCalc/MyCalc/CalcEngine.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalc
+{
+    class CalcEngine
+    {
+
+        //
+        // Operation Constants.
+        //
+        public enum Operator : int
+        {
+            eUnknown = 0,
+            eAdd = 1,
+            eSubtract = 2,
+            eMultiply = 3,
+            eDivide = 4
+        }
+
+
+        //
+        // Module-Level Constants
+        //
+
+        private static double negativeConverter = -1;
+        private static string versionInfo = "Calculator v2.0.1.1";
+
+        //
+        // Module-level Variables.
+        //
+
+        private static double numericAnswer;
+        private static string stringAnswer = "";
+        private static Operator calcOperation;
+        private static double firstNumber;
+        private static double secondNumber;
+        private static bool secondNumberAdded;
+        private static bool decimalAdded;
+
+        //
+        // Class Constructor.
+        //
+
+        public CalcEngine()
+        {
+            decimalAdded = false;
+            secondNumberAdded = false;
+        }
+
+        public static string Result { get; set; }
+
+        public static double Resultt { get; set; }
+
+        //
+        // Returns the custom version string to the caller.
+        //
+
+        public static string GetVersion()
+        {
+            return (versionInfo);
+        }
+        //
+        // Called when the Date key is pressed.
+        //
+
+        public static string GetDate()
+        {
+            DateTime curDate = new DateTime();
+            curDate = DateTime.Now;
+
+            stringAnswer = String.Concat(curDate.ToShortDateString(), " ", curDate.ToLongTimeString());
+
+            return (stringAnswer);
+        }
+
+        //
+        // Called when a number key is pressed on the keypad.
+        //
+
+        public static string CalcNumber(string KeyNumber)
+        {
+            stringAnswer += KeyNumber;
+            return stringAnswer;
+        }
+
+        //
+        // Called when an operator is selected (+, -, *, /)
+        //
+
+        public static void CalcOperation(Operator calcOper)
+        {
+            if (stringAnswer != "" && !secondNumberAdded)
+            {
+                NumberFormatInfo p = new NumberFormatInfo();
+                p.NumberDecimalSeparator = ",";
+                firstNumber = Convert.ToDouble(stringAnswer, p);
+                // if (stringAnswer.Contains(",")) { }
+                calcOperation = calcOper;
+                stringAnswer = "";
+                decimalAdded = false;
+            }
+        }
+
+        //
+        // Called when the +/- key is pressed.
+        //
+
+        public static string CalcSign()
+        {
+            double numHold;
+
+            if (stringAnswer != "")
+            {
+                numHold = System.Convert.ToDouble(stringAnswer);
+                stringAnswer = System.Convert.ToString(numHold * negativeConverter);
+            }
+
+            return (stringAnswer);
+        }
+
+        //
+        // Called when the . key is pressed.
+        //
+
+        public static string CalcDecimal()
+        {
+            if (!decimalAdded && !secondNumberAdded)
+            {
+                if (stringAnswer != "")
+                {
+                    stringAnswer += ".";
+                }
+                else
+                {
+                    stringAnswer = "0.";
+                }
+
+                decimalAdded = true;
+
+            }
+
+            return stringAnswer;
+        }
+        //
+        // Called when the factorial key is pressed
+        //
+        public static string CalcFactorial()
+        {
+            int numHold;
+            if (stringAnswer != "" && !stringAnswer.Contains(".") && !stringAnswer.Contains(",") && !stringAnswer.Contains("-"))
+            {
+                NumberFormatInfo p = new NumberFormatInfo();
+                p.NumberDecimalSeparator = ",";
+                numHold = Convert.ToInt32(stringAnswer, p);
+                int fact = 1;
+                for (int i = numHold; i > 0; i--)
+                {
+                    fact *= i;
+                }
+                stringAnswer = System.Convert.ToString(fact);
+            }
+            else
+            {
+                stringAnswer = "Mistake";
+            }
+
+            return stringAnswer;
+        }
+
+        public static string CalcSqrt()
+        {
+            double numHold;
+            if (stringAnswer != "")
+            {
+                numHold = Math.Sqrt(Convert.ToDouble(stringAnswer));
+                stringAnswer = numHold.ToString();
+            }
+            return stringAnswer;
+        }
+
+        public static string CalcPow()
+        {
+            double numHold;
+            if (stringAnswer != "")
+            {
+                numHold = Math.Pow(Convert.ToDouble(stringAnswer), 2);
+                stringAnswer = numHold.ToString();
+            }
+            return stringAnswer;
+        }
+        //
+        // Called when = is pressed.
+        //
+
+        public static string CalcEqual()
+        {
+            bool validEquation = false;
+
+            if (stringAnswer != "")
+            {
+                NumberFormatInfo p = new NumberFormatInfo();
+                p.NumberDecimalSeparator = ",";
+                secondNumber = Convert.ToDouble(stringAnswer, p);
+                secondNumberAdded = true;
+
+                switch (calcOperation)
+                {
+                    case Operator.eUnknown:
+                        validEquation = false;
+                        break;
+
+                    case Operator.eAdd:
+                        firstNumber += secondNumber;
+                        numericAnswer = firstNumber;
+                        validEquation = true;
+                        break;
+
+                    case Operator.eSubtract:
+                        numericAnswer = firstNumber - secondNumber;
+                        //Resultt = numericAnswer;
+                        validEquation = true;
+                        break;
+
+                    case Operator.eMultiply:
+                        numericAnswer = firstNumber * secondNumber;
+                        validEquation = true;
+                        break;
+
+                    case Operator.eDivide:
+                        numericAnswer = firstNumber / secondNumber;
+                        validEquation = true;
+                        break;
+
+                    default:
+                        validEquation = false;
+                        break;
+                }
+
+                if (validEquation)
+                    stringAnswer = System.Convert.ToString(numericAnswer);
+            }
+            // double.TryParse(stringAnswer, out firstNumber);
+            return stringAnswer;
+        }
+
+        //
+        // Resets the various module-level variables for the next calculation.
+        //
+
+        public static void CalcReset()
+        {
+            numericAnswer = 0;
+            firstNumber = 0;
+            secondNumber = 0;
+            stringAnswer = Result;
+            calcOperation = Operator.eUnknown;
+            decimalAdded = false;
+            secondNumberAdded = false;
+        }
+    }
+}
---
+++ MyCalc/MyCalc/MyCalc/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCalc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button factBut;
+        Button sqrt;
+        Button pow;
+        public static InputGestureCollection coll;
+        // Output Display Constants.
+        private const string oneOut = "1";
+        private const string twoOut = "2";
+        private const string threeOut = "3";
+        private const string fourOut = "4";
+        private const string fiveOut = "5";
+        private const string sixOut = "6";
+        private const string sevenOut = "7";
+        private const string eightOut = "8";
+        private const string nineOut = "9";
+        private const string zeroOut = "0";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            OutputDisplay.Text = "0";
+
+            InputGestureCollection coll = new InputGestureCollection();
+            coll.Add(new KeyGesture(Key.A, ModifierKeys.Control, "Ctrl+A"));
+        }
+
+        public static RoutedCommand HelpCmd = new RoutedCommand("About", typeof(MainWindow), coll);
+
+
+        private void OutputDisplay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OutputDisplay.Focus();
+        }
+
+        private void itemHelp_Click(object sender, RoutedEventArgs e)
+        {
+            //  MessageBox.Show("Программа вычисления \nпростых арифметических операций", "Калькулятор", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Программа вычисления \nпростых арифметических операций", "Калькулятор", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_2(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Программа вычисления \nпростых арифметических операций", "Калькулятор", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void myGrid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement fEl = e.Source as FrameworkElement;
+
+            switch (fEl.Name)
+            {
+                case "KeyDate":
+                    OutputDisplay.Text = CalcEngine.GetDate();
+                    CalcEngine.CalcReset();
+                    break;
+                case "KeyExit":
+                    this.Close();
+                    break;
+                case "KeyPlus":
+                    CalcEngine.CalcOperation(CalcEngine.Operator.eAdd);
+                    break;
+                case "KeyMinus":
+                    CalcEngine.CalcOperation(CalcEngine.Operator.eSubtract);
+                    break;
+                case "KeySign":
+                    OutputDisplay.Text = CalcEngine.CalcSign();
+                    break;
+                case "KeyDivide":
+                    CalcEngine.CalcOperation(CalcEngine.Operator.eDivide);
+                    CalcEngine.Result = OutputDisplay.Text;
+                    break;
+                case "KeyMultiply":
+                    CalcEngine.CalcOperation(CalcEngine.Operator.eMultiply);
+                    break;
+                case "KeyClear":
+                    CalcEngine.CalcReset();
+                    OutputDisplay.Text = "0";
+                    CalcEngine.Result = OutputDisplay.Text;
+                    break;
+                case "KeyPoint":
+                    OutputDisplay.Text = CalcEngine.CalcDecimal();
+                    break;
+                case "KeyEqual":
+                    OutputDisplay.Text = CalcEngine.CalcEqual();
+                    CalcEngine.Result = OutputDisplay.Text;
+                    CalcEngine.CalcReset();
+                    break;
+                case "KeyZero":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(zeroOut);
+                    break;
+                case "KeyOne":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(oneOut);
+                    break;
+                case "KeyTwo":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(twoOut);
+                    break;
+                case "KeyThree":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(threeOut);
+                    break;
+                case "KeyFour":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(fourOut);
+                    break;
+                case "KeyFive":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(fiveOut);
+                    break;
+                case "KeySix":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(sixOut);
+                    break;
+                case "KeySeven":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(sevenOut);
+                    break;
+                case "KeyEight":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(eightOut);
+                    break;
+                case "KeyNine":
+                    OutputDisplay.Text = CalcEngine.CalcNumber(nineOut);
+                    break;
+            }
+            e.Handled = true;
+        }
+
+        public void ExpandedView()
+        {
+            Expander e = new Expander();
+
+            factBut = new Button();
+            factBut.Content = "n!";
+            factBut.Click += FactBut_Click;
+
+            sqrt = new Button();
+            sqrt.Content = "√";
+            sqrt.Click += Sqrt_Click;
+
+            pow = new Button();
+            pow.Content = "x^2";
+            pow.Click += Pow_Click;
+
+            var newColumn = new ColumnDefinition { Width = GridLength.Auto };
+            myGrid.ColumnDefinitions.Add(newColumn);
+
+            myGrid.Children.Add(factBut);
+            myGrid.Children.Add(sqrt);
+            myGrid.Children.Add(e);
+            e.Content = pow;
+            e.Header = "More";
+
+            Grid.SetColumn(factBut, 6);
+            Grid.SetRow(factBut, 2);
+            Grid.SetColumn(sqrt, 6);
+            Grid.SetRow(sqrt, 3);
+            Grid.SetColumn(e, 6);
+            Grid.SetRow(e, 4);
+        }
+
+        private void Sqrt_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcSqrt();
+        }
+
+        private void FactBut_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcFactorial();
+        }
+
+        private void expandedView_Click(object sender, RoutedEventArgs e)
+        {
+            this.Width = 600;
+            ExpandedView();
+        }
+
+        private void standardView_Click(object sender, RoutedEventArgs e)
+        {
+            myGrid.Children.Remove(factBut);
+            myGrid.Children.Remove(sqrt);
+        }
+
+        private void Pow_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcPow();
+        }
+    }
+
+}
